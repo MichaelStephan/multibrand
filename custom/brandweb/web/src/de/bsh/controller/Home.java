569094a29ea3dd4228a829bec804d09884539c27
Stephan <stephan.mic@gmail.com>
2014-09-11 16:00:45 +0200

    initial

---
+++ custom/brandweb/web/src/de/bsh/controller/Home.java
@@ -0,0 +1,16 @@
+package de.bsh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by i303874 on 9/11/14.
+ */
+@Controller
+public class Home {
+    @RequestMapping(value = "/")
+    public String home() {
+        return "home";
+    }
+
+}
---
+++ custom/bshmultibrandcontrollercommon/src/de/bsh/constants/BshmultibrandcontrollercommonConstants.java
@@ -0,0 +1,29 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2013 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ * 
+ *  
+ */
+package de.bsh.constants;
+
+/**
+ * Global class for all Bshmultibrandcontrollercommon constants. You can add global constants for your extension into this class.
+ */
+public final class BshmultibrandcontrollercommonConstants extends GeneratedBshmultibrandcontrollercommonConstants
+{
+	public static final String EXTENSIONNAME = "bshmultibrandcontrollercommon";
+
+	private BshmultibrandcontrollercommonConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
